6d248226b09ac5cdbaf621b4aec5dc22acef71d4
Godfrey96 <ngwatlegodfrey@gmail.com>
2023-04-28 15:43:13 +0200

    signup design complete

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/model/user.model';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signUpForm!: FormGroup;
+  isSubmitted = false;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this._initSignupForm();
+  }
+
+  _initSignupForm() {
+    this.signUpForm = this.fb.group({
+      username: ['', Validators.required],
+      phoneNo: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+
+    const user: User = {
+      username: this.signUpFormError['username'].value,
+      phoneNo: this.signUpFormError['phoneNo'].value,
+      email: this.signUpFormError['email'].value,
+      password: this.signUpFormError['password'].value,
+    }
+
+    console.log("New user: ", user);
+  }
+
+
+  // errors
+  get signUpFormError() {
+    return this.signUpForm.controls;
+  }
+
+}
---
+++ src/app/model/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+    // id?: string;
+    username?: string;
+    phoneNo?: string;
+    email?: string;
+    password?: string;
+}
