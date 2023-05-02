db219bed92ae2504686e334c85a26ac8a69bfc16
Godfrey96 <ngwatlegodfrey@gmail.com>
2023-05-02 11:14:24 +0200

    landing design complete

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { ListOfUsersComponent } from './list-of-users/list-of-users.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    ListOfUsersComponent
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule
-  ]
+  ],
+  // exports: [
+  //   DashboardComponent,
+  //   // ListOfUsersComponent
+  // ]
 })
 export class DashboardModule { }
