bd115459a7a35407efd3802f284333a7c4249fe7
MOGAU GODFREY NGWATLE <ngwatlegodfrey@gmail.com>
2023-05-18 22:23:29 +0200

    done with user dashboard and displaying total in admin dashbaord

--- src/app/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin-dashboard/admin-dashboard.component.ts
@@ -13,7 +13,76 @@ import Swal from 'sweetalert2';
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
 
+  totalAdmins: number = 0;
+  totalUsers: number = 0;
+  totalActive: number = 0;
+  totalDisable: number = 0;
+
+  constructor(
+    private userService: UserService,
+    private notificationService: NotificationService
+  ) { }
+
+  ngOnInit(): void {
+
+    // fetching total number of admins
+    this._fetchingTotalAdmins();
+
+    // fetching total number of users
+    this._fetchingTotalUsers();
+    // fetching total number of active
+    this._fetchingTotalActive();
+
+    // fetching total number of disable
+    this._fetchingTotalDisable();
+
+
+  }
+
+  _fetchingTotalAdmins() {
+    this.userService.getTotalAdmins().subscribe({
+      next: (res: any) => {
+        this.totalAdmins = res;
+      },
+      error: () => {
+        this.notificationService.showError("Failed to fetch total number of admins", "ERROR")
+      }
+    });
+  }
+
+  _fetchingTotalUsers() {
+    this.userService.getTotalUsers().subscribe({
+      next: (res: any) => {
+        this.totalUsers = res;
+      },
+      error: () => {
+        this.notificationService.showError("Failed to fetch total number of users", "ERROR")
+      }
+    });
+  }
+
+  _fetchingTotalActive() {
+    this.userService.getTotalActive().subscribe({
+      next: (res: any) => {
+        this.totalActive = res;
+      },
+      error: () => {
+        this.notificationService.showError("Failed to fetch total number of active users/admins", "ERROR")
+      }
+    });
+  }
+
+  _fetchingTotalDisable() {
+    this.userService.getTotalDisable().subscribe({
+      next: (res: any) => {
+        this.totalDisable = res;
+      },
+      error: () => {
+        this.notificationService.showError("Failed to fetch total number of deactive users/admins", "ERROR")
+      }
+    });
+  }
 
 }
